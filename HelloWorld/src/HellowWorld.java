962d14267074a957b14f769db1e09bcf36f65927
Craig Ballingall <swampy990@gmail.com>
2016-10-15 13:46:39 +0100

    Oracle book on Java sample code thing

--- HelloWorld/src/HellowWorld.java
+++ HelloWorld/src/HellowWorld.java
@@ -1,8 +1,15 @@
 
 public class HellowWorld {
 
+	/*
+	 * This is a simple Java Program
+	 * 
+	 * I'm supposed to call this file example.java - but I probably won't
+	 */
+	
+	// A Java Program begins with call to main()
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Wibble");
 	}
 
