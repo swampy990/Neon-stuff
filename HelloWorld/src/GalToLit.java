59a2cb20e989058c6cbe91043f017d08644adb34
Craig Ballingall <swampy990@gmail.com>
2016-10-16 11:53:46 +0100

    More advanced

--- HelloWorld/src/GalToLit.java
+++ HelloWorld/src/GalToLit.java
@@ -1,22 +1,29 @@
 
+/*
+ * This creates a conversion table for Gallons to Litres
+ */
+
 public class GalToLit {
-
 	public static void main(String[] args) {
-		// Variable Section
 		
-		double	gals ; //Hold the number of Gallons
-		//double litres; //Hold the number of Litres 
-		double GalPerLiter; //conversion ratio
+	double Gals, Litres, conv; 	//Holds Gallons and Liters values
+	int counter;				// Integer to do counting
+	
+	counter = 0;
+	conv = 3.7854;
+	
+	for (Gals = 1; Gals <=100; Gals ++ ) {
+			Litres = Gals * conv;
+			System.out.println(Gals + "Gallons is " + Litres + " Litres");
 		
-		gals=99;
-		GalPerLiter  = 3.7854;
-		
-		System.out.println( gals + " Gallons is equal to " + gals * GalPerLiter )  ;
-		
-		
-		
-
-
+			counter ++;
+			
+			if (counter == 10) {
+			
+				System.out.println("...");
+				counter = 0;
+			}
+		}
 	}
-
 }
+	
