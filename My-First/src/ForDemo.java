f78a9302979b9b4da9ddbf7780c1d1d4b2592716
Craig Ballingall <swampy990@gmail.com>
2016-10-16 10:58:24 +0100

    more lessons

---
+++ My-First/src/ForDemo.java
@@ -0,0 +1,12 @@
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		int count;
+		
+		for (count = 0; count < 50; count = count + 1)
+			System.out.println("Hannah Smells: " + count);
+		System.out.println("All done");
+	}
+
+}
