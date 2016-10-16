f13a943c3c4ab70980af58d87a1cb8842781a7ea
Craig Ballingall <swampy990@gmail.com>
2016-10-16 01:51:28 +0100

    added new class

---
+++ HelloWorld/src/GalToLit.java
@@ -0,0 +1,22 @@
+
+public class GalToLit {
+
+	public static void main(String[] args) {
+		// Variable Section
+		
+		double	gals ; //Hold the number of Gallons
+		//double litres; //Hold the number of Litres 
+		double GalPerLiter; //conversion ratio
+		
+		gals=99;
+		GalPerLiter  = 3.7854;
+		
+		System.out.println(  gals * GalPerLiter )  ;
+		
+		
+		
+
+
+	}
+
+}
--- HelloWorld/src/HellowWorld.java
+++ HelloWorld/src/HellowWorld.java
@@ -1,16 +1,9 @@
 
 public class HellowWorld {
 
-	/*
-	 * This is a simple Java Program
-	 * 
-	 * I'm supposed to call this file example.java - but I probably won't
-	 */
-	
-	// A Java Program begins with call to main()
-
 	public static void main(String[] args) {
-		System.out.println("Wibble");
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 	}
 
 }
