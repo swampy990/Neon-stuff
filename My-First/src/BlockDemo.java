ddf34d1459f8066bea7bf7bee562cb672b3b80bd
Craig Ballingall <swampy990@gmail.com>
2016-10-18 00:15:53 +0100

    fgf

---
+++ My-First/src/BlockDemo.java
@@ -0,0 +1,20 @@
+
+public class BlockDemo {
+
+	public static void main(String[] args) {
+		
+		double i,j,d; 
+		
+		i = 5;
+		j = 10;
+		
+		//start the code block
+		if (i != 0) {
+			System.out.println("I does not equal ZERO");
+			d = j / i;
+			
+			System.out.println("J Divided by I is equal to " + d );
+		}
+	}
+
+}
